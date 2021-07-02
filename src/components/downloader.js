400e66e864263168921c397d79817578be511e9d
Abhishek Rajpoot <itsabhishekrajpoot@gmail.com>
2021-07-02 16:49:16 +0530

    loader new 1

--- src/components/downloader.js
+++ src/components/downloader.js
@@ -43,7 +43,7 @@ export default class ButtonLoader extends Component {
     //Faking API call here
     setTimeout(() => {
       this.setState({ loading: false });
-    }, 2000);
+    }, 200);
     
   };
   
@@ -113,7 +113,80 @@ const downloadVideo = () => {
               style={{ marginRight: "5px" }}
             />
           )}
-          {loading && <span>Loading...</span>}
+          {loading && <span>Loading... <span>
+  <svg width="28" viewBox="0 0 100 60">
+    <rect x="0" y="0" width="10" height="60" rx="6">
+      <animate attributeType="CSS" 
+               attributeName="height" 
+               values="60;20;60;"
+               begin='0s'
+               dur="1s"
+               repeatCount="indefinite" />
+      <animate attributeType="CSS" 
+               attributeName="y" 
+               begin='0s'
+               values="0;20;0;"
+               dur="1s"
+               repeatCount="indefinite" />
+    </rect>
+    <rect x="20" y="0" width="10" height="60" rx="6">
+      <animate attributeType="CSS" 
+               attributeName="height" 
+               values="60;20;60"
+               begin='0.2s'
+               dur="1s"
+               repeatCount="indefinite" />
+      <animate attributeType="CSS" 
+               attributeName="y" 
+               values="0;20;0"
+               begin='0.2s'
+               dur="1s"
+               repeatCount="indefinite" />
+    </rect>
+    <rect x="40" y="0" width="10" height="60" rx="6">
+      <animate attributeType="CSS" 
+               attributeName="height" 
+               values="60;20;60"
+               begin='0.4s'
+               dur="1s"
+               repeatCount="indefinite" />
+      <animate attributeType="CSS" 
+               attributeName="y" 
+               values="0;20;0"
+               begin='0.4s'
+               dur="1s"
+               repeatCount="indefinite" />
+    </rect>
+    <rect x="60" y="0" width="10" height="60" rx="6">
+      <animate attributeType="CSS" 
+               attributeName="height" 
+               values="60;20;60"
+               begin='0.6s'
+               dur="1s"
+               repeatCount="indefinite" />
+      <animate attributeType="CSS" 
+               attributeName="y" 
+               values="0;20;0"
+               begin='0.6s'
+               dur="1s"
+               repeatCount="indefinite" />
+    </rect>
+    <rect x="80" y="0" width="10" height="60" rx="6">
+      <animate attributeType="CSS" 
+               attributeName="height" 
+               values="60;20;60"
+               begin='0.8s'
+               dur="1s"
+               repeatCount="indefinite" />
+      <animate attributeType="CSS" 
+               attributeName="y" 
+               values="0;20;0"
+               begin='0.8s'
+               dur="1s"
+               repeatCount="indefinite" />
+    </rect>
+  </svg>
+</span></span>}
           {!loading && <span>Download</span>}
         </button>
           </Col>
