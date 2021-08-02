480b088fc41fd864662e77596e273e2fee64fe9e
iabhishekrajpoot <84550714+iabhishekrajpoot@users.noreply.github.com>
2021-08-02 16:03:27 +0530

    api

--- src/components/downloader.js
+++ src/components/downloader.js
@@ -19,7 +19,7 @@ export default class ButtonLoader extends Component {
   };
 
   fetchData = async() => {
-   await fetch("https://instareelsdownload.com/json?url="+this.state.url)
+   await fetch("https://api.instavideosave.net/videoInfo?videoURL="+this.state.url)
       .then(res => res.json())
       .then(
         (result) => {
@@ -65,10 +65,10 @@ export default class ButtonLoader extends Component {
     
 //  };
 const downloadVideo = () => {
-        window.open(`https://instareelsdownload.com/json?url=${this.state.url}`, "_self");     
+        window.open(`https://api.instavideosave.net/download?videoURL=${this.state.url}`, "_self");     
     }
     const downloadAudio = () => {
-        window.open(`https://instareelsdownload.com/json?url=${this.state.url}`, "_self");     
+        window.open(`https://api.instavideosave.net/audio/download?videoURL=${this.state.url}`, "_self");     
     }
    // let color = items.statu < 10 ? 'zero' : (items.statu <= 30 ? 'searchbtn' : 'searchbtn');
 
